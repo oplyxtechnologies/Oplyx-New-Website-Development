8211362e42818d463825b8662708d5d866d0f72a
oplyxtechnologies <oplyxtechnologies@gmail.com>
2025-07-07 16:51:04 +0545

    build error fix &apos;

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -9,7 +9,8 @@ export default function NotFound() {
         </h1>
         <h2 className="text-3xl font-bold mb-4">Page Not Found</h2>
         <p className="text-white/70 mb-8">
-          The page you’re looking for doesn’t exist or has been moved.
+          The page you&apso;re looking for doesn&appos;t exist or has been
+          moved.
         </p>
 
         <Link href="/">
